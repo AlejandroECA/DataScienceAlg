844be749d21ba56fdd40337bf53ada85ad6b1ef4
Alejandro Carrillo <alejandroecarrilloa@gmail.com>
2021-01-28 17:04:19 -0600

    added persistor to the cart

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import { Provider } from 'react-redux';
+import {store,persistor} from './redux/store';
+
+import { PersistGate } from 'redux-persist/integration/react'; 
+
 
 ReactDOM.render(
   <Provider store={store} >
     <React.StrictMode>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
