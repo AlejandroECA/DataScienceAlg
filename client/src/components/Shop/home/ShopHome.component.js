192f225f405d9691c40f477cc648a87df8dbf4f5
Alejandro Carrillo <alejandroecarrilloa@gmail.com>
2021-07-06 10:56:51 -0500

    prepare for the animation

--- client/src/components/Shop/home/ShopHome.component.js
+++ client/src/components/Shop/home/ShopHome.component.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React , { Profiler } from 'react';
 import Directory from '../shop.components/directory/directory.component'
 import DefaulTitle from '../../title/title.component';
 
 import {HomeShopContainer} from './shopHome.styles'
 
 const ShopHome = () => {
+
     // throw Error
+
+    //  Testing component --- Profiler
+
+
     return(
         <HomeShopContainer>
 
             <DefaulTitle title={'Shop'} />
-            <Directory />
+
+            
+            <Profiler id="Directory" onRender={(id,phase,actualDuration) => {
+                console.log({
+                    id,phase,actualDuration
+                });
+            }}>
+                <Directory />
+            </Profiler>
             
         </HomeShopContainer>
     )
