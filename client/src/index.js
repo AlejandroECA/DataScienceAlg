76dcf388848ffc12b081c907e6d6de79b8a70cc5
Alejandro Carrillo <alejandroecarrilloa@gmail.com>
2021-07-22 14:25:01 -0500

    updating the code to pwa, added serviceWorker, https(using express-sslify), updeted manifest

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,8 @@ import {store,persistor} from './redux/store';
 
 import { PersistGate } from 'redux-persist/integration/react'; 
 
+import * as serviceWorker from './serviceWorkerRegistration'
+
 
 ReactDOM.render(
   <Provider store={store} >
@@ -24,4 +26,7 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+serviceWorker.register()
+
 reportWebVitals();
--- server.mjs
+++ server.mjs
@@ -5,6 +5,8 @@ import dotenv from 'dotenv'
 import stripe from 'stripe'
 import compression from 'compression'
 
+import enforce from 'express-sslify'
+
 if (process.env.NODE_ENV !== 'production'){
     dotenv.config()
 }
@@ -19,9 +21,13 @@ const port = process.env.PORT || 5000
 app.use(compression())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader:true }))
 
 
 if(process.env.NODE_ENV === 'production'){
+    
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
     app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*',function(req,res){
@@ -37,6 +43,10 @@ app.get('/',function(req,res){
 app.listen(port, error =>{
     if(error) throw error;
     console.log('Server runing on port ' + port);
+})
+
+app.get('/service-worker.js',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, '..','build','service-worker.js'))
 })
 
 app.post('/payment', (req, res) => {
